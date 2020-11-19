ed3fd3efa3a7971dccab8f30b6c24dc503158366
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-11-19 16:57:38 +0000

    Exercise5

--- src/Run.java
+++ src/Run.java
@@ -1,22 +1,27 @@
 public class Run {
+    static int playerX = 0;
+    static int playerY = 0;
+    static String playerName = "Bubbles";
+    static int gameTick = 0;
+
+    static boolean iterate() {
+        boolean gameEnded = false;
+        playerX++;
+        playerY++;
+        gameTick++;
+        if (playerX == 5 && playerY == 5) {
+            gameEnded = true;
+        }
+        System.out.println("T: " + gameTick + "; Ended? " + gameEnded + "; player: " + playerName + "; position:(" + playerX + "," + playerY + ")");
+        return gameEnded;
+    }
 
     public static void main(String[] args) {
-        int playerX = 0;
-        int playerY = 0;
-        String playerName = "Bubbles";
-
-        int gameTick = 0;
         boolean gameEnded = false;
         System.out.println("T: " + gameTick + "; Ended? " + gameEnded + "; player: " + playerName + "; position:(" + playerX + "," + playerY + ")");
 
         while(!gameEnded) {
-            playerX++;
-            playerY++;
-            gameTick++;
-            if (playerX == 5 && playerY == 5) {
-                gameEnded = true;
-            }
-            System.out.println("T: " + gameTick + "; Ended? " + gameEnded + "; player: " + playerName + "; position:(" + playerX + "," + playerY + ")");
+            gameEnded = iterate();
         }
     }
 }
